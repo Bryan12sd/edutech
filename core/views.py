26d67d44332d31b3ac0d674bf1663dcf396a372b
Bryan Cordero <bryanzeta17@gmail.com>
2026-04-30 19:48:31 -0500

    Fix: aplicar .gitignore correctamente

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Estudiante ,Calificacion
 
-# Create your views here.
+def login_view(request):
+    return render(request, 'login.html')
+
+
+def dashboard(request):
+    estudiantes = Estudiante.objects.all()
+    return render(request, 'dashboard.html', {'estudiantes': estudiantes})
+
+def calificaciones(request):
+    calificaciones = Calificacion.objects.select_related('estudiante', 'curso')
+    return render(request, 'calificaciones.html', {'calificaciones': calificaciones})
